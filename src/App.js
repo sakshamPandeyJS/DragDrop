087e556102c62caf847edc25ac2c93fadbb516e9
Saksham Pandey <saksham.pandey@in.ey.com>
2021-12-01 10:18:06 +0530

    made new branch and add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import './App.css';
+import userdata from "./tempData.json";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [users, setUsers] = useState(userdata.data);
+  //console.log(users);
+
+  const handleDragEnd = (results)=>{
+    console.log("results",results.destination);
+    if(!results.destination)return;
+    const tempuser=[...users];
+    let [selectedRow]=tempuser.splice(results.source.index,1);
+    //console.log(selectedRow);
+    tempuser.splice(results.destination.index,0,selectedRow);
+    setUsers(tempuser);
+  }
+ return (
+   <DragDropContext onDragEnd={(results)=>handleDragEnd(results)}>
+    <div className="App mt-4">
+        <table  className="table borderd">
+          <thead className="head">
+            <tr>
+           <th scope="col">Drop</th>
+           <th scope="col">Username</th>
+           <th scope="col">Age</th>
+           <th scope="col">Gender</th>
+            </tr>
+          </thead>
+         
+         <Droppable droppableId="tbody">
+          
+           {(provided)=>( 
+             <tbody ref={provided.innerRef} {...provided.droppableProps} className="text-capitalize">
+               {
+                 users?.map((user, index) => (
+                  <Draggable draggableId={user.name} index={index} key={user.name}> 
+                     {
+                       (provided)=>(
+                         <tr ref={provided.innerRef}
+                           {...provided.draggableProps}
+                           >
+                           <td {...provided.dragHandleProps}> = </td>
+                           <td>{user.name}</td>
+                           <td>{user.age}</td>
+                           <td>{user.gender}</td>
+                           
+                         </tr>
+                        
+                       )
+                     }
+                   
+                   </Draggable>
+
+                 ))
+               
+             }  
+               {provided.placeholder}
+                </tbody>
+ )}
+             
+
+          
+         </Droppable>
+              
+           
+       
+        </table>
+  
     </div>
+    </DragDropContext>
   );
 }
 
